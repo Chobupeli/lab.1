2e2b3b808b5ad1f21275b0fec68a4ea1327b852e
dtlre <dtlre@XMXAXRXKX>
2026-02-06 17:42:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1chzh/ConsoleApplication1chzh.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int number;
+    cout << "Введите целое число в десятичной системе: ";
+    cin >> number;
+    int count = 0;
+    int numb2 = number; 
+    while (numb2 != 0) {
+        if (numb2 & 1) {
+            count++;
+        }
+        numb2 >>= 1;
+    }
+    cout << "Количество единиц в двоичном представлении: " << count << endl;
+    return 0;
+}
